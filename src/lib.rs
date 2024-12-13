4479dc4b4df06dabdcb38a2e8758d13a990b645c
lyh <liaoyinghai@yymt.com>
2024-12-13 13:41:44 +0800

    submit

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,15 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
-use syn::{DeriveInput};
 use quote::quote;
+use syn::{parse_macro_input, ItemFn};
 
-
-
-#[proc_macro_derive(MyDerive)]
-pub fn my_derive(ast: &DeriveInput) -> TokenStream {
-
-
-
-    let tokens = quote! {
-        struct Hello;
+#[proc_macro_attribute]
+pub fn log_handler(_attr: proc_macro::TokenStream, item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input_fn = parse_macro_input!(item as ItemFn);
+    let generate = quote! {
+            //调用属性宏
+            println!("log proc_macro_attribute is call !");
+        #input_fn
     };
-
-    tokens.into()
+    generate.into()
 }
-
